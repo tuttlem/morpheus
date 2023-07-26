0e6a25034688b459d7f633cdc615243ad32e0cda
Michael Tuttle <michael.tuttle@firstmac.com.au>
2023-07-26 20:39:16 +1000

    Added extra methods for Stadiums

--- backend/server/routes/stadiums.py
+++ backend/server/routes/stadiums.py
@@ -12,6 +12,32 @@ def list_stadiums():
     return make_response(stadiums, 200)
 
 
+@app.route('/stadiums/<stadium_id>', methods=["GET"])
+def get_stadium(stadium_id):
+    record = Stadium.query.get(stadium_id)
+    stadium = stadium_schema.dump(record)
+    return make_response(stadium, 200)
+
+
+@app.route('/stadiums/<int:id>', methods=["PUT"])
+def update_stadium(id):
+    stadium = Stadium.query.get(id)
+
+    if not stadium:
+        return jsonify({'message': 'Stadium not found'}), 404
+
+    updated_stadium_data = request.json
+    errors = stadium_schema.validate(updated_stadium_data, partial=True)
+
+    if errors:
+        return jsonify(errors), 400
+
+    for key, value in updated_stadium_data.items():
+        setattr(stadium, key, value)
+
+    db.session.commit()
+    return stadium_schema.jsonify(stadium), 200
+
 
 @app.route('/stadiums', methods=["POST"])
 def create_stadium():
@@ -23,3 +49,4 @@ def create_stadium():
     db.session.commit()
 
     return make_response({ "id": record.id }, 201)
+
